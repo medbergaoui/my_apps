92d3c2b26a12bff3ff007b4060e947dc4fd7626e
medbergaoui <57031259+medbergaoui@users.noreply.github.com>
2019-11-13 17:25:31 +0100

    main activity

---
+++ app/src/main/java/com/example/carmaintenanceschedule/Change_schedule_activity.java
@@ -0,0 +1,14 @@
+package com.example.carmaintenanceschedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Change_schedule_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_schedule_activity);
+    }
+}
