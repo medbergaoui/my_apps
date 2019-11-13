624d992f5a96b276a794f35f2b7b1e8c061bf7c1
medbergaoui <57031259+medbergaoui@users.noreply.github.com>
2019-11-13 17:38:12 +0100

    create account activity

---
+++ app/src/main/java/com/example/carmaintenanceschedule/Createaccount_activity.java
@@ -0,0 +1,14 @@
+package com.example.carmaintenanceschedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Createaccount_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createaccount_activity);
+    }
+}
